fb3a89904d233c03cadf8ba2fed9d91067b09fd2
taukir <taukir00sms@gmail.com>
2023-12-09 14:30:49 +0530

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
 import Footer from './Components/Footer/Footer'
 import Dashboard from './Components/Dashboard/Dashboard'
+import Stepper from './Components/ListingPage/Stepper/Stepper'
 
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      {/* <Navbar/>
       <Home/>
-      <Footer/>
+      <Footer/> */}
       {/* <Dashboard/> */}
+      <Stepper/>
      
   
      
--- src/Components/ListingPage/Stepper/Stepper.tsx
+++ src/Components/ListingPage/Stepper/Stepper.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Stepper as FormStepper, Step } from "react-form-stepper";
+import { GrHomeRounded } from "react-icons/gr";
+import sale from "../../../Images/Sale.png";
+import Rent from "../../../Images/Rent.png";
+import Residential from "../../../Images/Residential.png";
 
 const Stepper = () => {
+  const steps = [
+    { label: "Property details", icon: <GrHomeRounded /> },
+    { label: "Additional Details", icon: <GrHomeRounded /> },
+    { label: "Amenties", icon: <GrHomeRounded /> },
+  ];
+
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div>
+        <FormStepper activeStep={0}>
+          {steps.map((step, index) => (
+            <Step key={index} label={step.label}>
+              {step.icon}
+            </Step>
+          ))}
+        </FormStepper>
+      </div>
+      {/* after stepper componenet */}
 
-export default Stepper
+      <div className="pl-8 flex flex-col gap-5">
+        <div>
+          <h1 className="font-semibold text-[20px]">
+            Provide Details About Your Property
+          </h1>
+        </div>
+        <div>
+          <span>Property Listing For</span>
+          <div className="flex gap-4 text-center ">
+            <div className="flex p-2 px-6 bg-black rounded-md gap-3">
+              <div>
+                <img src={sale} alt="" />
+              </div>
+              <div className="text-white">Sale</div>
+            </div>
+            <div className="flex p-2 px-4  rounded-md gap-3 border border-black">
+              <div>
+                <img src={Rent} alt="" />
+              </div>
+              <div className="text-black">Rent</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Stepper;
